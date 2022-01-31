726ef59e730a0510f946876b1455189dad1f618d
NickLouis <nicktolmie86@gmail.com>
2022-01-31 12:34:55 +0000

    Added code for the end of call back vid

--- Callbacks.js
+++ Callbacks.js
@@ -1,14 +1,5 @@
-function taskOne(){
-    console.log("Starting task one");
-}
-function taskTwo(){
-    console.log("Starting task two");
-}
-
-//setTimeout(taskOne,2000);
-//taskOne()
-//taskTwo();
-
-//const message = () =>{console.log("This message is a delayed message")} 
-
-setTimeout(() =>{console.log("This message is a delayed message")} ,2000);
+window.onload = () => {
+  document.querySelector("#but").addEventListener("click", function () {
+    alert("User has clicked a button");
+  });
+};
