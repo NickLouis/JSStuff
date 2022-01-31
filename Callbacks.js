4d7bfb2f036b34f8289761391912123f176c596e
NickLouis <nicktolmie86@gmail.com>
2022-01-31 11:56:21 +0000

    Adding Callbacks tutorial file

---
+++ Callbacks.js
@@ -0,0 +1,14 @@
+function taskOne(){
+    console.log("Starting task one");
+}
+function taskTwo(){
+    console.log("Starting task two");
+}
+
+//setTimeout(taskOne,2000);
+//taskOne()
+//taskTwo();
+
+//const message = () =>{console.log("This message is a delayed message")} 
+
+setTimeout(() =>{console.log("This message is a delayed message")} ,2000);
